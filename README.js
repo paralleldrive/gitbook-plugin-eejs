b78cedfea6b375550ff87c099df88ac89d1c7696
Eric Elliott <dilvie@dilvie.com>
2015-05-15 13:41:55 -0600

    Update README.js

--- README.js
+++ README.js
@@ -4,6 +4,15 @@ Custom style and behavior for Learn JavaScript with Eric Elliott.
 
 ## Installing
 
+First, install GitBook:
+
+```
+$ npm install -g gitbook-cli
+```
+
+That will give you the `gitbook` command that you'll use to build the book or serve it on your local machine for preview.
+
+
 This plugin supplies the styles for all of the EEJS courses. It is a GitBook plugin. 
 
 ```
