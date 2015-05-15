9e84b6fd3999cde709dcf03049b098665d55bf47
Eric Elliott <dilvie@dilvie.com>
2015-05-15 13:12:50 -0600

    Update README.js

--- README.js
+++ README.js
@@ -1,3 +1,12 @@
-## gitbook-plugin-eejs
+# gitbook-plugin-eejs
 
 Custom style and behavior for Learn JavaScript with Eric Elliott.
+
+## Installing
+
+This plugin supplies the styles for all of the EEJS courses. It is a GitBook plugin. 
+
+```
+$ git clone git@github.com:paralleldrive/gitbook-plugin-eejs.git
+```
+
